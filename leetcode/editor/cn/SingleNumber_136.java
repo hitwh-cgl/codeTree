269169d2a5b023c0582f1cce182a53bfa6d0e8e9
liuchenguang002 <liuchenguang002@ke.com>
2022-05-29 13:55:16 +0800

    只出现一次的元素；

---
+++ leetcode/editor/cn/SingleNumber_136.java
@@ -0,0 +1,36 @@
+package cn;
+
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,1]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: [4,1,2,1,2]
+//输出: 4 
+// Related Topics 位运算 数组 
+// 👍 2430 👎 0
+
+
+public class SingleNumber_136 {
+    static //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int sum = 0;
+            for (int num : nums) {
+                sum ^= num;
+            }
+            return sum;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
