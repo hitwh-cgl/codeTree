fb1feeb8f3a605708a7783b97bec188dd315910f
liuchenguang <17862701770@163.com>
2022-07-06 19:19:11 +0800

    中等 递归

---
+++ leetcode/editor/cn/DecodeString_394.java
@@ -0,0 +1,107 @@
+package cn;
+
+//给定一个经过编码的字符串，返回它解码后的字符串。
+//
+// 编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。 
+//
+// 你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。 
+//
+// 此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "3[a]2[bc]"
+//输出："aaabcbc"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "3[a2[c]]"
+//输出："accaccacc"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "2[abc]3[cd]ef"
+//输出："abcabccdcdcdef"
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：s = "abc3[cd]xyz"
+//输出："abccdcdcdxyz"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 30 
+// s 由小写英文字母、数字和方括号 '[]' 组成 
+// s 保证是一个 有效 的输入。 
+// s 中所有整数的取值范围为 [1, 300] 
+// 
+// Related Topics 栈 递归 字符串 
+// 👍 1207 👎 0
+
+
+public class DecodeString_394 {
+    public static void main(String[] args) {
+        String s = "3[a]2[bc]";
+        s = "3[a2[c]]";
+        s = "30[a2[c]]";
+        s = "2[abc]3[cd]ef";
+        s = "abc3[cd]xyz";
+        Solution solution = new Solution();
+        System.out.println(solution.decodeString(s));
+    }
+
+    static //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // "3[a]2[bc]"
+        // "3[a2[c]]"
+        // "2[abc]3[cd]ef"
+        // "abc3[cd]xyz"
+        public String decodeString(String s) {
+            StringBuilder builder = new StringBuilder();
+            int times = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char cur = s.charAt(i);
+                if (cur >= '0' && cur <= '9') {
+                    times = times * 10 + Integer.parseInt(cur + "");
+                } else if (cur != '[') {
+                    builder.append(cur);
+                    times = 0;
+                } else {
+                    int open = 1;
+                    int end = i;
+                    while (open != 0) {
+                        end++;
+                        if (s.charAt(end) == ']') {
+                            open--;
+                        } else if (s.charAt(end) == '[') {
+                            open++;
+                        }
+                    }
+                    String sub = s.substring(i + 1, end);
+                    String subString = decodeString(sub);
+                    for (int j = 1; j <= times; j++) {
+                        builder.append(subString);
+                    }
+                    times = 0;
+                    i = end;
+                }
+            }
+            return builder.toString();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
